17847dedbeefdf3cd85ea08a418825c5f1c8abaa
Yawksh <yawukalfentaw@gmail.com>
2025-05-13 12:16:21 +0300

    added rank()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,14 +9,22 @@ GROUP BY
 ORDER BY 
     total_bookings DESC;
 
--- Window Function: Rank properties based on total number of bookings using ROW_NUMBER
+-- Window Function: Use ROW_NUMBER to rank properties by total bookings
 SELECT 
     property_id,
     COUNT(*) AS booking_count,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number_rank
 FROM 
     bookings
 GROUP BY 
-    property_id
-ORDER BY 
-    rank;
+    property_id;
+
+-- Window Function: Use RANK to handle ties in ranking of properties
+SELECT 
+    property_id,
+    COUNT(*) AS booking_count,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM 
+    bookings
+GROUP BY 
+    property_id;
