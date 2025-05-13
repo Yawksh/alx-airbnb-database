f9d32252139bfecda21b646ddf80b29e155079a5
Yawksh <yawukalfentaw@gmail.com>
2025-05-13 12:19:57 +0300

    explian analyze

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,30 @@
--- Indexes on high-usage columns
-CREATE INDEX idx_user_id ON bookings(user_id);
-CREATE INDEX idx_property_id ON bookings(property_id);
-CREATE INDEX idx_review_property_id ON reviews(property_id);
-CREATE INDEX idx_start_date ON bookings(start_date);
+-- BEFORE indexing
+EXPLAIN ANALYZE
+SELECT * FROM bookings
+WHERE user_id = 5;
+
+EXPLAIN ANALYZE
+SELECT * FROM properties
+WHERE city = 'Addis Ababa';
+
+EXPLAIN ANALYZE
+SELECT * FROM users
+WHERE email = 'test@example.com';
+
+-- CREATE indexes on high-usage columns
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_properties_city ON properties(city);
+CREATE INDEX idx_users_email ON users(email);
+
+-- AFTER indexing
+EXPLAIN ANALYZE
+SELECT * FROM bookings
+WHERE user_id = 5;
+
+EXPLAIN ANALYZE
+SELECT * FROM properties
+WHERE city = 'Addis Ababa';
+
+EXPLAIN ANALYZE
+SELECT * FROM users
+WHERE email = 'test@example.com';
